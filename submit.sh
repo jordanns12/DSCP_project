9bed82d8e0cba03564373d06ce2083f1f97cf504
jordanns12 <103905480+jordanns12@users.noreply.github.com>
2024-04-29 13:36:33 -0500

    Update submit.sh

--- submit.sh
+++ submit.sh
@@ -8,7 +8,7 @@ rm -rf slurm_out; mkdir slurm_out
 # Submit an array job for processing data from 2018 to 2024
 jobId1=$(sbatch --output="slurm_out/slurm-%A_%a.out" \
                --error="slurm_out/slurm-%A_%a.err" \
-               --array=2022-2024 \
+               --array=2018-2024 \
                getData.sh)
 
 jobId1=$(echo $jobId1 | sed 's/Submitted batch job //')
